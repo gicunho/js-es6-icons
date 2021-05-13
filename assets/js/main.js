11017fe75f9d372c196b47e56adf4d7cb5b07ca2
gicunho <gicunho@hotmail.it>
2021-05-13 19:27:00 +0200

    Complete Milestone 1

---
+++ assets/js/main.js
@@ -0,0 +1,115 @@
+const icone = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	}
+];
+
+//Milestone 1:
+//Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+
+icone.forEach((elemento, index) => {
+    document.getElementById('icons').insertAdjacentHTML('beforeend', `
+    <div class="my-3 d-flex flex-column justify-content-center align-items-center">
+    <i class="${elemento.family} ${elemento.prefix}${elemento.name} fa-2x"></i>
+    <span class="text-uppercase d-block">${elemento.name}</span>
+    </div>
+    `)
+})
+//Milestone 2 
+//Coloriamo le icone per tipo
+
+//Milestone 3 
+//Creiamo una select con i tipi di icone e usiamola per filtrare le icone
